5fed0cad40a5ce82477c61b11eb9f0361e9bb171
Ninkov <ninkov82@gmail.com>
2024-07-23 00:24:42 +0300

    Convert and separate html into components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
-
-
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
 function App() {
-  
-
   return (
-    <>
-     <h1>Start</h1>
-    </>
-  )
+    <div id="box">
+      <Header />
+      <main id="main-content">
+        <Home />
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
